d7b9829d2103e9b9d450e2edab552298411d3a47
Gerben Neven <gerbyzation@me.com>
2017-07-30 14:57:03 +0100

    setup tests

---
+++ project/tests/test_users.py
@@ -0,0 +1,16 @@
+# project/tests/test_users.py
+import json
+
+from project.tests.base import BaseTestCase
+
+
+class TestUsersService(BaseTestCase):
+    """Tests for the Users Service."""
+
+    def test_users(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
