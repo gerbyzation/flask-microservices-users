9b728a51613246f4d1862d390821acc6f460d84c
Gerben Neven <gerbyzation@me.com>
2017-08-21 00:35:03 +0100

    add test coverage, read DB url from env

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -1,5 +1,6 @@
 # project/tests/test_config.py
 import unittest
+import os
 
 from flask import current_app
 from flask_testing import TestCase
@@ -18,10 +19,9 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
-        print(app.config['SQLALCHEMY_DATABASE_URI'])
         self.assertTrue(
             app.config['SQLALCHEMY_DATABASE_URI'] ==
-            'postgres://postgres:postgres@users-db:5432/users_dev'
+            os.environ.get('DATABASE_URL')
         )
 
 
@@ -36,8 +36,8 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(
-            app.config['SQLALCHEMY_DATABASE_URI'] == 
-            'postgres://postgres:postgres@users-db:5432/users_test'
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            os.environ.get('DATABASE_TEST_URL')
         )
 
 
