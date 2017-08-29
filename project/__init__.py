680cae3941cf26805a3cf173c45979db0274d714
Gerben Neven <gerbyzation@me.com>
2017-08-29 16:33:52 +0100

    add migrations, make username and email unique

--- project/__init__.py
+++ project/__init__.py
@@ -4,10 +4,12 @@ import datetime
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 # instantiate the db
 db = SQLAlchemy()
-
+# instantiate flask migrate
+migrate = Migrate()
 
 def create_app():
     # instantiate the app
@@ -22,6 +24,7 @@ def create_app():
 
     # set up extensions
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.views import users_blueprint
---
+++ project/tests/test_user_model.py
@@ -0,0 +1,48 @@
+from sqlalchemy.exc import IntegrityError
+
+from project import db
+from project.api.models import User
+from project.tests.base import BaseTestCase
+
+
+class TestUserModel(BaseTestCase):
+    def test_add_user(self):
+        user = User(
+            username='justatest',
+            email='test@test.com',
+        )
+        db.session.add(user)
+        db.session.commit()
+        self.assertTrue(user.id)
+        self.assertEqual(user.username, 'justatest')
+        self.assertEqual(user.email, 'test@test.com')
+        self.assertTrue(user.active)
+        self.assertTrue(user.created_at)
+
+    def test_add_user_duplicate_username(self):
+        user = User(
+            username='justatest',
+            email='test@test.com',
+        )
+        db.session.add(user)
+        db.session.commit()
+        duplicate_user = User(
+            username='justatest',
+            email='test@test2.com',
+        )
+        db.session.add(duplicate_user)
+        self.assertRaises(IntegrityError, db.session.commit)
+
+    def test_add_user_duplicate_email(self):
+        user = User(
+            username='justatest',
+            email='test@test.com',
+        )
+        db.session.add(user)
+        db.session.commit()
+        duplicate_user = User(
+            username='justanothertest',
+            email='test@test.com',
+        )
+        db.session.add(duplicate_user)
+        self.assertRaises(IntegrityError, db.session.commit)
